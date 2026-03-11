6f5ef6840db9f93b150c73d396a2ab59b2c3cae8
Fernando Oliveira da Cruz <66073979+Fernando-Cruz@users.noreply.github.com>
2026-03-11 10:19:59 -0300

    Primeiro Update, e Criações de Entidades.
    Criação da aplica e com Spring Initialize e criação das Entidades (Coupon, Address e Event).

---
+++ api/src/test/java/com/fernandoCruz/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoCruz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
